b8eafc60f1046bfb14014de60a02753e3ad8d7e9
teknomines <abdulkarim@teknomines.com>
2024-02-23 19:20:28 +0530

    cronjob run

--- app/Console/Commands/inActiveUser.php
+++ app/Console/Commands/inActiveUser.php
@@ -28,9 +28,46 @@ class inActiveUser extends Command
     
     public function handle()
     {
-        $data = User::with('life_insurance','vehicle_insurance','mutual_fund','mediclaim','roles')->whereHas('roles', function($query) {
+        $users = User::with('life_insurance','vehicle_insurance','mutual_fund','mediclaim','roles')->whereHas('roles', function($query) {
             $query->where('name','member');
         })->get();
+        foreach($users[0]->vehicle_insurance as $us){
+            $created_at = $us->created_at;
+            $futureDate=date($created_at, strtotime('+1 year'));
+            if(strtotime($created_at)==strtotime($futureDate)){
+                $user_update = User::whereId($us->user_id)->update([
+                    'isActive' => 1,
+                ]);
+            }
+        }
         
+        foreach($users[0]->life_insurance as $life_ins){
+            $created_at = $life_ins->created_at;
+            $futureDate=date($created_at, strtotime('+1 year'));
+            if(strtotime($created_at)==strtotime($futureDate)){
+                $user_update = User::whereId($life_ins->user_id)->update([
+                    'isActive' => 1,
+                ]);
+            }
+        }
+
+        foreach($users[0]->mediclaim as $medi){
+            $created_at = $medi->created_at;
+            $futureDate=date($created_at, strtotime('+1 year'));
+            if(strtotime($created_at)==strtotime($futureDate)){
+                $user_update = User::whereId($medi->user_id)->update([
+                    'isActive' => 1,
+                ]);
+            }
+        }
+        foreach($users[0]->mutual_fund as $mf){
+            $created_at = $mf->created_at;
+            $futureDate=date($created_at, strtotime('+1 year'));
+            if(strtotime($created_at)==strtotime($futureDate)){
+                $user_update = User::whereId($mf->user_id)->update([
+                    'isActive' => 1,
+                ]);
+            }
+        }
     }
 }
