21d1d8cd7670475e10787d08dae0fc71941208ec
teknomines <abdulkarim@teknomines.com>
2024-02-27 18:45:56 +0530

    added

--- app/Console/Commands/inActiveUser.php
+++ app/Console/Commands/inActiveUser.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
-
+Use Carbon;
 use App\Models\User;
 class inActiveUser extends Command
 {
@@ -31,43 +31,60 @@ class inActiveUser extends Command
         $users = User::with('life_insurance','vehicle_insurance','mutual_fund','mediclaim','roles')->whereHas('roles', function($query) {
             $query->where('name','member');
         })->get();
-        foreach($users[0]->vehicle_insurance as $us){
-            $created_at = $us->created_at;
-            $futureDate=date($created_at, strtotime('+1 year'));
-            if(strtotime($created_at)==strtotime($futureDate)){
-                $user_update = User::whereId($us->user_id)->update([
-                    'isActive' => 1,
-                ]);
-            }
-        }
         
-        foreach($users[0]->life_insurance as $life_ins){
-            $created_at = $life_ins->created_at;
-            $futureDate=date($created_at, strtotime('+1 year'));
-            if(strtotime($created_at)==strtotime($futureDate)){
-                $user_update = User::whereId($life_ins->user_id)->update([
-                    'isActive' => 1,
-                ]);
-            }
+        foreach($users[0]->vehicle_insurance as $us){
+            $created_at = strtotime($us->created_at);
+            $time = date('Y-m-d H:i:s');
+            $new_time = $created_at + (5 * 60);
+            $futureDate =date("Y-m-d H:i:s", $new_time);
+            do {
+                if(strtotime($futureDate) == strtotime($time)){
+                    $user_update = User::whereId($us->user_id)->update([
+                        'isActive' => 1,
+                    ]);
+                }
+            }while(strtotime($time) == strtotime($futureDate));
+        }
+        foreach($users[0]->life_insurance as $li){
+            $created_at = strtotime($li->created_at);
+            $time = date('Y-m-d H:i:s');
+            $new_time = $created_at + (5 * 60);
+            $futureDate =date("Y-m-d H:i:s", $new_time);
+            do {
+                if(strtotime($futureDate) == strtotime($time)){
+                    $user_update = User::whereId($li->user_id)->update([
+                        'isActive' => 1,
+                    ]);
+                }
+            }while(strtotime($time) == strtotime($futureDate));
         }
 
-        foreach($users[0]->mediclaim as $medi){
-            $created_at = $medi->created_at;
-            $futureDate=date($created_at, strtotime('+1 year'));
-            if(strtotime($created_at)==strtotime($futureDate)){
-                $user_update = User::whereId($medi->user_id)->update([
-                    'isActive' => 1,
-                ]);
-            }
+        foreach($users[0]->mediclaim as $med){
+            $created_at = strtotime($med->created_at);
+            $time = date('Y-m-d H:i:s');
+            $new_time = $created_at + (5 * 60);
+            $futureDate =date("Y-m-d H:i:s", $new_time);
+            do {
+                if(strtotime($futureDate) == strtotime($time)){
+                    $user_update = User::whereId($med->user_id)->update([
+                        'isActive' => 1,
+                    ]);
+                }
+            }while(strtotime($time) == strtotime($futureDate));
         }
+        
         foreach($users[0]->mutual_fund as $mf){
-            $created_at = $mf->created_at;
-            $futureDate=date($created_at, strtotime('+1 year'));
-            if(strtotime($created_at)==strtotime($futureDate)){
-                $user_update = User::whereId($mf->user_id)->update([
-                    'isActive' => 1,
-                ]);
-            }
+            $created_at = strtotime($mf->created_at);
+            $time = date('Y-m-d H:i:s');
+            $new_time = $created_at + (5 * 60);
+            $futureDate =date("Y-m-d H:i:s", $new_time);
+            do {
+                if(strtotime($futureDate) == strtotime($time)){
+                    $user_update = User::whereId($mf->user_id)->update([
+                        'isActive' => 1,
+                    ]);
+                }
+            }while(strtotime($time) == strtotime($futureDate));
         }
     }
 }
